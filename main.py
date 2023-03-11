af281bf75293268880ca6f34d0fc8349c37a506d
SaishaK <119130532+SaishaK@users.noreply.github.com>
2023-03-11 18:15:58 +0000

    Add: input for matrix

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("linear algebra")
+rows = int(input("Enter the number of rows"))
+columns = int(input("Enter the number of columns"))
+
+matrix = []
+
+for i in range(rows):
+    row = []
+    for j in range(columns):
+        value = int(input(f"Enter value for {i + 1, j + 1}"))
+        row.append(value)
+    matrix.append(row)
+
+for i in matrix:
+    print(i)
+    
